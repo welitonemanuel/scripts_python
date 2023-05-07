a1fddb9ab49eba8c656fa24563f7e6a9fceecd1f
wleite <welitonemanuel@gmail.com>
2023-05-07 17:36:00 -0300

    atualização da classe medidor

--- ClasseMedidor.py
+++ ClasseMedidor.py
@@ -4,31 +4,38 @@ class Medidor:
         Inicialização da classe Medidor
 
         Args:
-            em (str): número do medidor
-            dt_leitura (str): data da última leitura do medidor
-            leitura_kwh (float): valor da última leitura do medidor em kWh
-            autocad (bool): indica se o medidor está em autocadastro
-            status_rele (bool): indica o status do relé do medidor
-            cs_associada (str): nome da CS associada ao medidor
-            net_associada (str): nome da rede associada ao medidor
-            cliente_associado (str): nome do cliente associado ao medidor
-            dt_ult_leitura (str): data da penúltima leitura do medidor (opcional)
+            em (int): número do medidor
+            dt_leitura (date): data da última leitura do medidor
+            leitura_kwh (int): valor da última leitura do medidor em kWh
+            autocad (str): indica se o medidor está em autocadastro
+            status_rele (str): indica o status do relé do medidor
+            cs_associada (int(3)): nome da CS associada ao medidor
+            net_associada (int(4)): nome da rede associada ao medidor
+            cliente_associado (int(10)): nome do cliente associado ao medidor
+            dt_ult_leitura (date): data da penúltima leitura do medidor (opcional)
         """
-        self._em = em
-        self._dt_leitura = dt_leitura
-        self._leitura_kwh = leitura_kwh
-        self._autocad = autocad
-        self._status_rele = status_rele
-        self._cs_associada = cs_associada
-        self._net_associada = net_associada
-        self._cliente_associado = cliente_associado
-        self._dt_ult_leitura = dt_ult_leitura
+        self.instalacao = instalacao
+        self.em = em
+        self.net = net
+        self.cs = cs
+        self.dt_ult_leitura = dt_ult_leitura
+        self.dt_leitura = dt_leitura
+        self.status_rele = status_rele
+        self.leitura_kwh = leitura_kwh
+        self.autocad = autocad
 
     def __str__(self):
-        """
-        Método especial que retorna uma string com informações do medidor
-        """
-        return f"Medidor {self._em}: {self._leitura_kwh} kWh ({self._dt_leitura}), Cliente: {self._cliente_associado}, CS: {self._cs_associada}, Rede: {self._net_associada}, Status Relé: {'ON' if self._status_rele else 'OFF'}, {'Autocad' if self._autocad else ''}"
+        # Método especial que retorna uma string com informações do medidor
+        # return f"Medidor {self._em}: {self._leitura_kwh} kWh ({self._dt_leitura}), Cliente: {self._cliente_associado}, CS: {self._cs_associada}, Rede: {self._net_associada}, Status Relé: {'ON' if self._status_rele else 'OFF'}, {'Autocad' if self._autocad else ''}"
+        print("Instalacao: ", self._instalacao)
+        print("Medidor: ", self._em)
+        print("Net: ", self._net)
+        print("CS: ", self._cs)
+        print("Data da última leitura: ", self._dt_ult_leitura)
+        print("Data da leitura: ", self._dt_leitura)
+        print("Leitura em kWh: ", self._leitura_kwh)
+        print("Status do Relê: ", self._status_rede)
+        print("Autocadastro: ", self._autocad)
 
     def status_em_d2(self):
         """
