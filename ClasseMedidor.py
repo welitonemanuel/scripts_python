2382fac6172c0b901e93462c5e3a80aa73e232bf
wleite <welitonemanuel@gmail.com>
2023-05-07 18:03:17 -0300

    atualização da classe medidor

--- ClasseMedidor.py
+++ ClasseMedidor.py
@@ -27,7 +27,7 @@ class Medidor:
     def __str__(self):
         # Método especial que retorna uma string com informações do medidor
         # return f"Medidor {self._em}: {self._leitura_kwh} kWh ({self._dt_leitura}), Cliente: {self._cliente_associado}, CS: {self._cs_associada}, Rede: {self._net_associada}, Status Relé: {'ON' if self._status_rele else 'OFF'}, {'Autocad' if self._autocad else ''}"
-        print("Instalação:", self._instalacao)
+        print("Instalação: ", self._instalacao)
         print("Medidor: ", self._em)
         print("Net: ", self._net)
         print("CS: ", self._cs)
@@ -37,23 +37,28 @@ class Medidor:
         print("Status do Relê: ", self._status_rede)
         print("Autocadastro: ", self._autocad)
 
-    def status_em_d2(self):
+    def status_em_d2(self, dias:int) -> str:
         """
-        Verifica se o medidor está desconectado
+        Método que retorna o status do relé do medidor em, baseado na diferença entre as últimas duas leituras.
 
-        Returns:
-            str: retorna "ON" se o medidor estiver conectado, "OFF" caso contrário
+        Se a diferença for menor que dias, retorna 'ON'. Se for maior que dias, retorna 'OFF'.
+        Se dt_ult_leitura for nulo, retorna 'OFF'.
+
+        :param dias: quantidade máxima de dias para a diferença entre as leituras ser considerada 'ON'
+        :return: string indicando o status do relé do medidor em
         """
-        if not self._dt_ult_leitura:
-            return "OFF"
-        diferenca = (datetime.strptime(self._dt_leitura, '%d/%m/%Y') - datetime.strptime(self._dt_ult_leitura, '%d/%m/%Y')).days
-        if diferenca < 2:
+        if not dias:
+            dias = 2
+        try:
+            dias = int(dias)
+            if dias <= 0:
+                raise ValueError("O número de dias deve ser positivo.")
+        except ValueError:
+            print("O número de dias informado não é um inteiro positivo.")
+            return None
+        if not self._dt_ult_leitura: return "OFF"
+        diff = (self.dt_leitura - self.dt_ult_leitura).days
+        if diff < 2:
             return "ON"
         else:
             return "OFF"
-
-    def obter_informacoes(self):
-        """
-        Método que imprime as informações do medidor
-        """
-        print(f"Medidor {self._em}: {self._leitura_kwh} kWh ({self._dt_leitura}), Cliente: {self._cliente_associado}, CS: {self._cs_associada}, Rede: {self._net_associada}, Status Relé: {'ON' if self._status_rele else 'OFF'}, {'Autocad' if self._autocad else ''}")
